c4581e8d44cdc4cda0c0762de72425f04e219edc
AlecLafita <alec_23394@hotmail.com>
2017-03-09 20:49:14 +0100

    Added the Mesh class and reorganized code

--- Assets/Scripts/CaveGenerator.cs
+++ Assets/Scripts/CaveGenerator.cs
@@ -5,61 +5,17 @@ using Geometry;
 
 public class CaveGenerator : MonoBehaviour {
 
-	List<int> mTriangles;
-	List<Vector3> mVertices;
-
+	Geometry.Mesh proceduralMesh;
 	public int extrudeTimes = 100;
 
 	bool hole = true; //TODO: change this!
-
-	/** Makes the triangulation from the triangle vertices**/
-	void triangulateTriangle(int v1, int v2, int v3) {
-		mTriangles.Add (v1); mTriangles.Add (v2);mTriangles.Add (v3);
-	}
-
-	/** Triangulates the quad iff is not part of a hole**/
-	void triangulateQuad(Vertex bl, Vertex br, Vertex tl, Vertex tr) {
-		/**		The quad vertices seen from outside the cave:
-		 * 		tl___tr
-		 * 		|	 |
-		 * 		bl___br
-		 **/
-		if (!((bl.getInHole() && tr.getInHole()) || (tl.getInHole() && br.getInHole()))) {
-			//Left-hand!
-			mTriangles.Add (bl.getIndex()); mTriangles.Add (tr.getIndex()); mTriangles.Add (tl.getIndex());
-			mTriangles.Add (bl.getIndex()); mTriangles.Add (br.getIndex()); mTriangles.Add (tr.getIndex());
-		}
-	}
-
-	/** Makes the triangulation between two polylines, checking they
-	 * have the same size **/
-	void triangulatePolylines(Polyline pol1, Polyline pol2) {
-		if (pol1.getSize () != pol2.getSize ()) {//TODO : throw exception
-			Debug.Log ("The two polylines do not have the same length!");
-			return;
-		}
-		for (int i = 0; i < pol1.getSize(); ++i) {
-			triangulateQuad(pol1.getVertex(i), pol1.getVertex(i+1), pol2.getVertex(i), pol2.getVertex(i+1));
-		}
-	}
-
-	/** Closes a polyline by triangulating all it's vertices with it's baricenter**/
-	void closePolyline(Polyline poly) {
-		Vector3 baricenter = poly.calculateBaricenter ();
-		int baricenterIndex = mVertices.Count;
-		mVertices.Add (baricenter);
-		Debug.Log (baricenter); 
-		for (int i = 0; i < poly.getSize(); ++i) {
-			triangulateTriangle (poly.getVertex (i).getIndex(), poly.getVertex (i + 1).getIndex(), baricenterIndex);
-		}
-	}
 
 
 	/** From the vertices of an existing polyline, it creates a new new one
 	 * with the same number of vertices and following some direction and at some distance**/
 	void extrude(Polyline originPoly,  Vector3 direction, int distance) {
-		if (extrudeTimes < 0) {
-			closePolyline(originPoly);
+		if (extrudeTimes < 0) { //Base case, triangulate the polyline as a polygon
+			proceduralMesh.closePolyline(originPoly);
 			return;
 		}
 		
@@ -69,9 +25,9 @@ public class CaveGenerator : MonoBehaviour {
 			//Add vertex to polyline
 			newPoly.extrudeVertex(i, originPoly.getVertex(i).getPosition(), direction,distance);
 			//Add index vertex to polyline
-			newPoly.getVertex(i).setIndex(mVertices.Count);
+			newPoly.getVertex(i).setIndex(proceduralMesh.getNumVertices());
 			//Add the new vertex to the mesh
-			mVertices.Add(newPoly.getVertex(i).getPosition());
+			proceduralMesh.addVertex(newPoly.getVertex(i).getPosition());
 		}
 
 		//Make holes: mark some vertices (from old and new polyline) and form a new polyline
@@ -95,30 +51,24 @@ public class CaveGenerator : MonoBehaviour {
 		}
 
 		//Triangulate from origin to new polyline as a tube/cave shape
-		triangulatePolylines (originPoly, newPoly);
+		proceduralMesh.triangulatePolylines (originPoly, newPoly);
 		--extrudeTimes;
 		extrude(newPoly,direction,distance);
 	}
 
 	public void startGeneration (InitialPolyline iniPol) {
-		mVertices = new List<Vector3>();
-		mTriangles = new List<int>();
-
-		//Add the first polyline vertices to the mesh
-		for (int i = 0; i < iniPol.getSize (); ++i) {
-			mVertices.Add (iniPol.getVertex (i).getPosition ());
-		}
+		//Create the mesh that will be modified during the cave generation
+		proceduralMesh = new Geometry.Mesh (iniPol);
 
 		//Start the generation
-		extrude (iniPol,  new Vector3 (0.0f, 0.0f, 1.0f), 10);
+		extrude (iniPol, new Vector3 (0.0f, 0.0f, 1.0f), 10);
 
-
-		//Assign the vertices and triangles created to a mesh
-		Mesh mesh = new Mesh ();
+		//Generation finished, assign the vertices and triangles created to a Unity mesh
+		UnityEngine.Mesh mesh = new UnityEngine.Mesh ();
 		//mesh.vertices = mVertices.ToArray(); //Slower
-		mesh.SetVertices (mVertices);
+		mesh.SetVertices (proceduralMesh.getVertices());
 		//mesh.triangles = mTriangles.ToArray ();
-		mesh.SetTriangles (mTriangles,0);
+		mesh.SetTriangles (proceduralMesh.getTriangles(),0);
 		mesh.RecalculateNormals ();
 		mesh.RecalculateBounds();
 
@@ -126,9 +76,10 @@ public class CaveGenerator : MonoBehaviour {
 		GetComponent<MeshFilter> ().mesh = mesh;
 	}
 		
-	void OnDrawGizmos() { //For debug purposes
+	//For debug purposes
+	void OnDrawGizmos() { 
 		if (!Application.isPlaying) return; //Avoid error messages after stopping
-		Vector3[] vertices = mVertices.ToArray ();
+		Vector3[] vertices = proceduralMesh.getVertices().ToArray ();
 		for (int i = 0; i < vertices.Length; ++i) {
 			Gizmos.DrawWireSphere (center: vertices [i], radius: 0.2f);
 		}
---
+++ Assets/Scripts/Geometry/Mesh.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geometry {
+	/** This class contains the mesh that will be dynamically generated. It's important then,
+	 * that the operations that modify it are done in constant time.
+	 * The triangles need to have the normal with the direction pointing to the inside 
+	 * of the cave, in order to see the triangulation from inside**/
+
+	public class Mesh {
+
+		private List<Vector3> mVertices; //The vertices position. There's no need to have them as a Vertex instances
+		private List<int> mTriangles; //The triangles that form the mesh. It has a size multiple of three as 
+									  //uses the vertices indices
+
+		//Constructors
+		public Mesh() {
+			mVertices = new List<Vector3>();
+			mTriangles = new List<int>();
+		}
+
+		/** Creates the mesh intializating it with a polyline's vertices**/
+		public Mesh (Polyline iniPol) {
+			mVertices = new List<Vector3>();
+			mTriangles = new List<int>();
+			//Add the polyline vertices to the mesh
+			for (int i = 0; i < iniPol.getSize (); ++i) {
+				mVertices.Add (iniPol.getVertex (i).getPosition ());
+			}
+		}
+
+		//Getters
+		public List<Vector3> getVertices() {
+			return mVertices;
+		}
+		public int getNumVertices() {
+			return mVertices.Count;
+		}
+
+		public List<int> getTriangles() {
+			return mTriangles;
+		}
+		public int getNumTriangles() {
+			return mTriangles.Count;
+		}
+
+		//Setters
+		public void setVertices(List<Vector3> vertices) {
+			mVertices = vertices;
+		}
+
+		public void setTriangles(List<int> triangles) {
+			mTriangles = triangles;
+		}
+
+		//Other functions
+		/** Adds a new vertex to the mesh **/
+		public void addVertex(Vector3 v) {
+			mVertices.Add (v);
+		}
+
+		/** Adds a new triangle to the mesh**/
+		private void addTriangle(int v1, int v2, int v3) {
+			mTriangles.Add (v1); mTriangles.Add (v2); mTriangles.Add (v3);
+		}
+
+		/** Triangulates the quad iff is not part of a hole**/
+		private void triangulateQuad(Vertex bl, Vertex br, Vertex tl, Vertex tr) {
+			/**		The quad vertices seen from outside the cave:
+			 * 		tl___tr
+			 * 		|	 |
+			 * 		bl___br
+			 **/
+			if (!((bl.getInHole() && tr.getInHole()) || (tl.getInHole() && br.getInHole()))) {
+				//Left-hand!
+				addTriangle(bl.getIndex(), tr.getIndex(), tl.getIndex());
+				addTriangle(bl.getIndex(), br.getIndex(), tr.getIndex());
+			}
+		}
+
+		/** Makes the triangulation between two polylines with same size. The order of
+		 * the vertices has to be the same. This is used then for neighbour polylines**/
+		public void triangulatePolylines(Polyline pol1, Polyline pol2) {
+			if (pol1.getSize () != pol2.getSize ()) { //TODO : throw exception
+				Debug.Log ("The two polylines do not have the same length!");
+				return;
+			}
+			for (int i = 0; i < pol1.getSize(); ++i) {
+				triangulateQuad(pol1.getVertex(i), pol1.getVertex(i+1), pol2.getVertex(i), pol2.getVertex(i+1));
+			}
+		}
+
+		/** Closes a polyline by triangulating all it's vertices with it's baricenter, visualizing it as a polygon**/
+		public void closePolyline(Polyline poly) {
+			Vector3 baricenter = poly.calculateBaricenter ();
+			int baricenterIndex = getNumVertices();
+			addVertex (baricenter);
+			Debug.Log (baricenter); 
+			for (int i = 0; i < poly.getSize(); ++i) {
+				//Left-hand!!
+				addTriangle(poly.getVertex (i).getIndex(), poly.getVertex (i + 1).getIndex(), baricenterIndex);
+			}
+		}
+
+	}
+}
